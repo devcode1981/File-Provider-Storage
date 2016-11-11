f68fe5b047b5e9361410378144dc36d889880c12
Hrvoje Marjanovic <hrvoje.marjanovic@crossvallia.hr>
2016-11-11 20:44:22 +0100

    final test

--- test_ubuntu.sh
+++ test_ubuntu.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
+
+set -e
+
 gem install gitlab-development-kit
 
 gdk init
 cd gitlab-development-kit
 gdk install
 support/set-gitlab-upstream
+
+gdk run &
+
+sleep 30
+
+wget http://localhost:3000/
 
