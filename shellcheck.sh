d37c7277b9c96184f5444d0bc9574a228e09be0a
Alessio Caiazza <acaiazza@gitlab.com>
2017-10-30 10:17:02 +0100

    Remove CI jobs superseded by codequality

--- shellcheck.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# finds all executables (any executable bit is set)
-# with a shell shebang that are not in .git
-# POSIX-compliant
-
-# shellcheck disable=SC2038
-find . ! -path "./.git/*" ! -path "./gitlab/*" -type f \( -perm -u=x -o -perm -g=x -o -perm -o=x \) -exec grep -l '^#!/bin/sh$' {} \+ | xargs shellcheck
