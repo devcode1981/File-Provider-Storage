d088a89c50d10db4cfb2a7ee8422fe2c8e1c22ce
Toon Claes <toon@gitlab.com>
2019-02-28 20:22:36 +0100

    Revert "Make gdk reconfigure less aggressive"
    This reverts commit 58eb0c9b753a8076e4225249d084366fbca69a95.

--- lib/gdk.rb
+++ lib/gdk.rb
@@ -69,7 +69,7 @@ module GDK
       true
     when 'reconfigure'
       remember!($gdk_root)
-      exec(MAKE, "ruby_version=#{RUBY_VERSION}", 'touch-examples', 'unlock-dependency-installers', 'postgresql-sensible-defaults', 'all', chdir: $gdk_root)
+      exec(MAKE, "ruby_version=#{RUBY_VERSION}", 'clean-config', 'unlock-dependency-installers', 'postgresql-sensible-defaults', 'all', chdir: $gdk_root)
     when 'psql'
       exec('psql', '-h', File.join($gdk_root, 'postgresql'), '-p', pg_port.to_s, *ARGV, chdir: $gdk_root)
     when 'redis-cli'
