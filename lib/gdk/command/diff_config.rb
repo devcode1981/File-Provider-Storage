1b6dc700782bdc50849fcb541312e00defb07278
Albert Salim <asalim@gitlab.com>
2019-12-06 11:16:44 +0800

    Move `ConfigDiff` into `GDK::Command::DiffConfig`

--- lib/gdk/command/diff_config.rb
+++ lib/gdk/command/diff_config.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require_relative '../../config_diff'
+require 'fileutils'
 
 module GDK
   module Command
@@ -33,6 +33,46 @@ module GDK
 
         file_diffs.each do |diff|
           stdout.puts diff.output unless diff.output == ""
+        end
+      end
+
+      private
+
+      class ConfigDiff
+        attr_reader :file, :output, :make_output
+
+        def initialize(file)
+          @file = file
+
+          execute
+        end
+
+        def file_path
+          @file_path ||= File.join($gdk_root, file)
+        end
+
+        private
+
+        def execute
+          FileUtils.mv(file_path, "#{file_path}.unchanged")
+
+          @make_output = update_config_file
+
+          @output = diff_with_unchanged
+        ensure
+          File.rename("#{file_path}.unchanged", file_path)
+        end
+
+        def update_config_file
+          run(GDK::MAKE, file)
+        end
+
+        def diff_with_unchanged
+          run('git', 'diff', '--no-index', '--color', "#{file}.unchanged", file)
+        end
+
+        def run(*commands)
+          IO.popen(commands.join(' '), chdir: $gdk_root, &:read).chomp
         end
       end
     end
