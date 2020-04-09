fe28e185d2ff12a1ea11809fbefdae42499a78ac
Gabriel Mazetto <brodock@gmail.com>
2020-04-09 03:42:30 +0200

    Use `gdk.example.yml` for testing
    This prevents specs from failing because they have different values than
    defaults on local machine

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,7 +6,10 @@ RSpec.configure do |config|
   config.before(:each) do
     allow($stdout).to receive(:puts)
     allow($stdout).to receive(:write)
+
+    # isolate configs for the testing environment
     allow(GDK).to receive(:root) { temp_path }
+    stub_const('GDK::Config::FILE', 'gdk.example.yml')
   end
 end
 
